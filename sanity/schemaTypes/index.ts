c9dd1fafd62e4e68a1d471cef5614ad33a777a1e
Heidi <emhp009@gmail.com>
2024-11-26 17:24:35 +0100

    salestype in schema added

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,10 +2,16 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import {blockContentType} from './blockContentType'
 import {categoryType} from './categoryType'
-import {postType} from './postType'
 
 import { productType } from './productType'
+import { orderType } from './orderType'
+import { salesType } from './salesType'
 
+
+// acts as a "table of contents" for the schemas. When Sanity initializes, it looks at this schema object to know:
+
+// What types of documents (like products, orders, or categories) you want to manage.
+// How these types are structured.
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, productType],
+  types: [blockContentType, categoryType,  productType, orderType, salesType],
 }
