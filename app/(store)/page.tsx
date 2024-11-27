c19b2dfe8319791bc3e3e6b3a7cbd1ac29e817ce
Heidi <emhp009@gmail.com>
2024-11-27 09:59:34 +0100

    up

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,15 +1,17 @@
 import { Button } from "@/components/ui/button";
-import { getAllProducts } from "@/sanity/lib/getAllProduct";
 
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
   return (
     <div>
-      <h1>Landing Page </h1>
+      <h1>Landing Page
 
-      <div>
-        <ProductsView products={products}/>
-      </div>
+
+ 
+      
+      
+      </h1>
 
       
     </div>
