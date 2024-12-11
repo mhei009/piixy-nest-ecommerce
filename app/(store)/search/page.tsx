3e67cdf2e00923716d6120fb15400192790dc84a
Heidi <emhp009@gmail.com>
2024-12-11 14:42:47 +0100

    search added working

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,3 +1,5 @@
+import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
+
 async function SearchPage({searchParams,
     
 
@@ -6,10 +8,25 @@ async function SearchPage({searchParams,
         query: string
     }; }) {
         const { query } = await searchParams;
+        const products = await searchProductsByName(query);
+
+        if (!products.length) {
 
   return (
-    <div>search page for {query} </div>
-  )
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-2">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-2xl font-bold mb-6 text-center">
+          No Products Found for: {query}
+        </h1>
+        <p className="text-grey-600 text-center">
+          Try seach with different keywords
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default SearchPage
+return <div>SearchPage for {query}</div>
+
+    }
+export default SearchPage;
--- sanity.types.ts
+++ sanity.types.ts
@@ -386,6 +386,81 @@ export type ALL_PRODUCTS_QUERYResult = Array<{
   stock?: number;
 }>;
 
+// Source: ./sanity/lib/products/searchProductsByName.ts
+// Variable: PRODUCT_SEARCH_QUERY
+// Query: *[        _type == "product"        && name match $searchParam    ] | order(name asc)
+export type PRODUCT_SEARCH_QUERYResult = Array<{
+  _id: string;
+  _type: "product";
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  slug?: Slug;
+  image?: {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: "image";
+  };
+  moreImages?: Array<{
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: "image";
+    _key: string;
+  }>;
+  price?: number;
+  description?: Array<{
+    children?: Array<{
+      marks?: Array<string>;
+      text?: string;
+      _type: "span";
+      _key: string;
+    }>;
+    style?: "blockquote" | "h1" | "h2" | "h3" | "h4" | "normal";
+    listItem?: "bullet";
+    markDefs?: Array<{
+      href?: string;
+      _type: "link";
+      _key: string;
+    }>;
+    level?: number;
+    _type: "block";
+    _key: string;
+  } | {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    alt?: string;
+    _type: "image";
+    _key: string;
+  }>;
+  categories?: Array<{
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    _key: string;
+    [internalGroqTypeReferenceTo]?: "category";
+  }>;
+  stock?: number;
+}>;
+
 // Source: ./sanity/lib/sale/getActiveSaleByCouponCode.ts
 // Variable: ACTIVE_SALE_BY_COUPON_QUERY
 // Query: * [        _type == "sale"         && isActive == true        && couponCode == $couponCode    ] | order(validFrom desc) [0]
@@ -411,6 +486,7 @@ declare module "@sanity/client" {
     "\n    *[_type == \"brand\"] | order(name asc)\n  ": ALL_BRAND_QUERYResult;
     "\n    *[\n        _type == \"category\"] | order(name asc)\n        ": ALL_CATEGORIES_QUERYResult;
     "\n    *[\n        _type == \"product\"] | order(name asc)\n        ": ALL_PRODUCTS_QUERYResult;
+    "\n    *[\n        _type == \"product\"\n        && name match $searchParam\n    ] | order(name asc)\n    ": PRODUCT_SEARCH_QUERYResult;
     "\n    * [\n        _type == \"sale\" \n        && isActive == true\n        && couponCode == $couponCode\n    ] | order(validFrom desc) [0]\n    ": ACTIVE_SALE_BY_COUPON_QUERYResult;
   }
 }
