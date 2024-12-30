d3c5b5f0483d71a5788c82ae77ab6b0f05a9ca14
Heidi <emhp009@gmail.com>
2024-12-30 04:26:48 +0100

    update, next to add the backend client

--- app/(store)/webhook/route.ts
+++ app/(store)/webhook/route.ts
@@ -1,3 +1,4 @@
+import { Metadata } from "@/actions/createCheckoutSession";
 import stripe from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
@@ -45,8 +46,53 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ received: true });
 }
-
-async function createOrderInSanity(session: Stripe.Checkout.Session) {
     // create order in Sanity
     // return order
+async function createOrderInSanity(session: Stripe.Checkout.Session) {
+
+    const {
+        id, 
+        amount_total,
+        currency,
+        metadata,
+        payment_intent,
+        customer,
+        total_details,
+    
+
+    } = session
+
+    const {
+        orderNumber,
+        customerName,
+        customerEmail,
+        clerkUserId,
+    } = metadata as Metadata;
+
+
+    // get information from checkoutchekout sessions to access metadata
+    const lineItemsWithProduct = await stripe.checkout.sessions.listLineItems(
+        id,
+        {
+            expand: ["dataset.price.product"],
+        }
+    );
+
+
+    //transforms each line item from the Stripe checkout session into a format 
+// that can be used to create order documents in Sanity CMS. For each item, it:
+//  makes a unique ID (_key) for the product.
+// adds a reference to the product (_ref) in Sanity, using the product's ID from Stripe.
+
+
+
+    const sanityProducts = lineItemsWithProduct.data.map ((item) => ({
+        _key: crypto.randomUUID(),
+        product: {
+            _type: "reference",
+            _ref: (item.price?.product as Stripe.Product).metadata?.id,
+        },
+        quantity: item.quantity || 0,
+    })
+    )
 }
